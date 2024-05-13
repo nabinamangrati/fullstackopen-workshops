c0e4ebcef872731ebc47c276f02e3cfb2f6808ff
nabinamangrati <mangratinabina1@gmail.com>
2024-05-13 14:33:41 +0545

    notes app aprt2 b

--- part2/notes-app/src/App.jsx
+++ part2/notes-app/src/App.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import Note from "./components/Notes";
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+
+  const notesToShow = notes.filter((note) => {
+    return !note.important;
+  });
+  const handleSubmit = () => {
+    event.preventDefault();
+    setNotes(
+      notes.concat({
+        content: newNote,
+        id: notes.length + 1,
+        important: Math.random() < 0.5,
+      })
+    );
+    setNewNote("");
+    console.log("form has been submitted");
+  };
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
   return (
     <>
       <h1>Notes</h1>
       <ul>
-        {notes.map((value) => {
+        {notesToShow.map((value) => {
+          {
+            /* {notes.map((value) => { */
+          }
           return <Note key={value.id} note={value} />;
         })}
         {/* yesto garda index.js ma thape paxi hample app.jsx ma pani thapnu 
@@ -13,6 +39,10 @@ const App = ({ notes }) => {
         <li>{props.notes[1].content}</li>
         <li>{props.notes[2].content}</li> */}
       </ul>
+      <form onSubmit={handleSubmit}>
+        <input value={newNote} onChange={handleChange} />
+        <button>submit</button>
+      </form>
     </>
   );
 };
