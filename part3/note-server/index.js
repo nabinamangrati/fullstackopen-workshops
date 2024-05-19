9c893664c4c2d666d1dda8b39677e5dbb85eb6ca
nabinamangrati <mangratinabina1@gmail.com>
2024-05-19 23:14:08 +0545

    part3 workshop get delete request

--- part3/note-server/index.js
+++ part3/note-server/index.js
@@ -1,5 +1,5 @@
-const http = require("http");
-
+const express = require("express");
+const app = express();
 let notes = [
   {
     id: 1,
@@ -17,10 +17,29 @@ let notes = [
     important: true,
   },
 ];
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/json" });
-  response.end(JSON.stringify(notes));
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const myId = Number(request.params.id);
+  const myNotes = notes.find((note) => note.id === myId);
+  if (myNotes) {
+    response.json(myNotes);
+  } else {
+    response.status(204).end(`there is no note on  ${myId}`);
+  }
+});
+app.delete("/api/notes/:id", (request, response) => {
+  const myId = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== myId);
+
+  response.status(204).end(`the note on id ${myId} has been deleted`);
 });
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
