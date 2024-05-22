af599b29ec24415b514b7e0869f89a7bd5a60e09
nabinamangrati <mangratinabina1@gmail.com>
2024-05-22 11:50:42 +0545

    new note added to mongo.js

--- part3/note-server/mongo.js
+++ part3/note-server/mongo.js
@@ -20,12 +20,19 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", noteSchema);
 
-const note = new Note({
+const note1 = new Note({
   content: "HTML is easy",
   important: true,
 });
+const note2 = new Note({
+  content: "Browser can execute only Javascript",
+  important: false,
+});
 
-note.save().then((result) => {
+note1.save().then((result) => {
+  console.log("note saved!");
+});
+note2.save().then((result) => {
   console.log("note saved!");
   mongoose.connection.close();
 });
