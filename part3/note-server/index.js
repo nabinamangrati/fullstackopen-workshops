2320784f7da3939c230718b1d48cf687115d017d
nabinamangrati <mangratinabina1@gmail.com>
2024-05-22 11:50:09 +0545

    mongo added to index.js

--- part3/note-server/index.js
+++ part3/note-server/index.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+const url = `mongodb+srv://nabina1:mypassword@cluster0.fjzyldz.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
 app.use(express.json());
 app.use(cors());
 app.use(express.static("dist"));
@@ -12,39 +35,54 @@ const requestLogger = (request, response, next) => {
   next();
 };
 app.use(requestLogger);
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
+let notes = [];
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id)
+    .then((result) => {
+      // const myId = Number(request.params.id);
+      // const myNote = notes.find((note) => note.id === myId);
+      if (result) {
+        response.json(result);
+      } else {
+        response.end(`there is no note on  ${request.params.id}`);
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+      response
+        .status(500)
+        .send(`${request.params.id} is not in the required format`);
+    });
+});
+
+app.put("/api/notes/:id", (request, response) => {
   const myId = Number(request.params.id);
-  const myNotes = notes.find((note) => note.id === myId);
-  if (myNotes) {
-    response.json(myNotes);
+  const updatedNote = request.body;
+  let noteFound = false;
+  notes = notes.map((note) => {
+    if (note.id !== myId) return note;
+    else {
+      noteFound = true;
+      return updatedNote;
+    }
+  });
+
+  if (noteFound) {
+    response.status(202).json(updatedNote);
   } else {
-    response.end(`there is no note on  ${myId}`);
+    response.status(404).send(`There are no notes at ${myId}`);
   }
 });
+
 app.delete("/api/notes/:id", (request, response) => {
   const myId = Number(request.params.id);
   notes = notes.filter((note) => note.id !== myId);
@@ -58,6 +96,7 @@ app.post("/api/notes/", (request, response) => {
   console.log(myNewPost);
   response.status(201).json(myNewPost);
 });
+
 app.use((request, response, next) => {
   response.status(404).send("no code available to handle this request");
 });
