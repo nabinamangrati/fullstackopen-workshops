5aa4970ecdd6bbfe5c2a1414905a7a518cfb95c9
nabinamangrati <mangratinabina1@gmail.com>
2024-05-27 11:00:05 +0545

    testing of the dummy function in workshops

---
+++ part3/note-server/tests/reverse.test.js
@@ -0,0 +1,20 @@
+const reverse = require("../utils/for_testing").reverse;
+const assert = require("assert");
+
+test("reverse of a", () => {
+  const result = reverse("a");
+
+  assert.strictEqual(result, "a");
+});
+
+test("reverse of react", () => {
+  const result = reverse("react");
+
+  assert.strictEqual(result, "tcaer");
+});
+
+test("reverse of saippuakauppias", () => {
+  const result = reverse("saippuakauppias");
+
+  assert.strictEqual(result, "saippuakauppias");
+});
