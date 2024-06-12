4212d67244f9da2af4c0c3abdb1138f534e17030
nabinamangrati <mangratinabina1@gmail.com>
2024-06-12 13:56:41 +0545

    in note authorization added in post

--- part2/notes-app/src/services/notes.js
+++ part2/notes-app/src/services/notes.js
@@ -4,8 +4,12 @@ const baseUrl = "/api/notes";
 const getAll = () => {
   return axios.get(baseUrl).then((result) => result.data);
 };
-const create = (note) => {
-  return axios.post(baseUrl, note);
+const create = (note, token) => {
+  return axios.post(baseUrl, note, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 };
 const update = (id, updatedNote) => {
   return axios.put(`${baseUrl}/${id}`, updatedNote);
