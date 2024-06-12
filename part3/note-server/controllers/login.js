61d7d6a5dafa0268ee6b99fd58878ed88ad04ba1
nabinamangrati <mangratinabina1@gmail.com>
2024-06-12 12:56:18 +0545

    token expiry time extended

--- part3/note-server/controllers/login.js
+++ part3/note-server/controllers/login.js
@@ -22,7 +22,7 @@ loginRouter.post("/", async (request, response) => {
   };
 
   const token = jwt.sign(userForToken, process.env.SECRET, {
-    expiresIn: 30,
+    expiresIn: 60 * 60 * 60,
   });
   console.log(token, "token");
 
