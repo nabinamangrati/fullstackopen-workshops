f46319339a7e2b6948f4abac7b1a7bc1129533d0
nabinamangrati <mangratinabina1@gmail.com>
2024-07-24 13:35:46 +0545

    redux used to set the counter

--- part6/redux-counter/src/index.jsx
+++ part6/redux-counter/src/index.jsx
@@ -1,21 +1,43 @@
 import { useState } from "react";
 import { createRoot } from "react-dom/client";
+import { createStore } from "redux";
 // import App from "./App";
+
+const counterReducer = (state = 0, action) => {
+  console.log(action, "action");
+  console.log(state, "state");
+  if (action.type === "ADD") {
+    const newState = state + 1;
+    return newState;
+  } else if (action.type === "SUBTRACT") {
+    const newState = state - 1;
+    return newState;
+  } else if (action.type === "ZERO") {
+    const newState = 0;
+    return newState;
+  }
+  return state;
+};
+const store = createStore(counterReducer);
 
 const App = () => {
   const [counter, setCounter] = useState(0);
   const addCounetr = () => {
-    setCounter(counter + 1);
+    // setCounter(counter + 1);
+    store.dispatch({ type: "ADD" });
   };
   const subtractCounter = () => {
-    setCounter(counter - 1);
+    // setCounter(counter - 1);
+    store.dispatch({ type: "SUBTRACT" });
   };
   const zeroCounter = () => {
-    setCounter(0);
+    // setCounter(0);
+    store.dispatch({ type: "ZERO" });
   };
   return (
     <div>
-      <div>{counter}</div>
+      {/* <div>{counter}</div> */}
+      <div>{store.getState()}</div>
       <button onClick={addCounetr}>add</button>
       <button onClick={subtractCounter}>subtract</button>
       <button onClick={zeroCounter}>zero</button>
@@ -25,3 +47,6 @@ const App = () => {
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
+store.subscribe(() => {
+  root.render(<App />);
+});
