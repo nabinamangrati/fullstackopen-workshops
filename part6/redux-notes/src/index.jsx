271e25063879a86ee478b5ad59762bede9989763
nabinamangrati <mangratinabina1@gmail.com>
2024-07-24 14:06:57 +0545

    simple redux-note

---
+++ part6/redux-notes/src/index.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { createRoot } from "react-dom/client";
+import { createStore } from "redux";
+// import App from "./App";
+
+const noteReducer = (state = [], action) => {
+  console.log(action, "action");
+  console.log(state, "state");
+  if (action.type === "NEW_NOTE") {
+    const newState = state.concat(action.payload);
+    return newState;
+  }
+
+  return state;
+};
+const store = createStore(noteReducer);
+store.dispatch({
+  type: "NEW_NOTE",
+  payload: {
+    content: "the app state is in redux store",
+    important: true,
+    id: 1,
+  },
+});
+store.dispatch({
+  type: "NEW_NOTE",
+  payload: {
+    content: "state changes are made with actions",
+    important: false,
+    id: 2,
+  },
+});
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? "important" : ""}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
+// store.subscribe(() => {
+//   root.render(<App />);
+// });
