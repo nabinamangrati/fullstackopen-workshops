f2b80e785c65172204c80768ec472295ab47d85a
nabinamangrati <mangratinabina1@gmail.com>
2024-07-24 11:15:21 +0545

    simple counter using state

--- part6/redux-counter/src/index.jsx
+++ part6/redux-counter/src/index.jsx
@@ -1,6 +1,27 @@
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+// import App from "./App";
 
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const addCounetr = () => {
+    setCounter(counter + 1);
+  };
+  const subtractCounter = () => {
+    setCounter(counter - 1);
+  };
+  const zeroCounter = () => {
+    setCounter(0);
+  };
+  return (
+    <div>
+      <div>{counter}</div>
+      <button onClick={addCounetr}>add</button>
+      <button onClick={subtractCounter}>subtract</button>
+      <button onClick={zeroCounter}>zero</button>
+    </div>
+  );
+};
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
