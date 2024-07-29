883c6ca78cab9c82657d89eea80125c0d41454a0
nabinamangrati <mangratinabina1@gmail.com>
2024-07-29 16:40:11 +0545

    vivsibility filter added

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,43 +1,10 @@
-// import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { filterChange } from "./reducers/filterReducer";
 import NoteForm from "./components/NoteForm";
 import Notes from "./components/Notes";
-
+import VisibilityFilter from "./components/VisibilityFilter";
 const App = () => {
-  // const [filter, setFilter] = useState("ALL");
-  const dispatch = useDispatch();
-  const filter = useSelector((state) => {
-    return state.filter;
-  });
-  const filterSelected = (filter) => {
-    console.log("clicked radio button", filter);
-    // setFilter(filter);
-    dispatch(filterChange(filter));
-  };
   return (
     <div>
-      <div>
-        all
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected("ALL")}
-          checked={filter === "ALL"}
-        />
-        important
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected("IMPORTANT")}
-        />
-        nonimportant
-        <input
-          type="radio"
-          name="filter"
-          onChange={() => filterSelected("NONIMPORTANT")}
-        />
-      </div>
+      <VisibilityFilter />
       <h1>Notes</h1>
       <NoteForm />
       <Notes />
