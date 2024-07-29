abae6f7920ddcc31f0e41a09b4e18971788fadb2
nabinamangrati <mangratinabina1@gmail.com>
2024-07-29 10:43:18 +0545

    radio button and its functionality added

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,11 +1,37 @@
+import { useState } from "react";
 import NoteForm from "./components/NoteForm";
 import Notes from "./components/Notes";
 const App = () => {
+  const [filter, setFilter] = useState("ALL");
+  const filterSelected = (filter) => {
+    console.log("clicked radio button", filter);
+    setFilter(filter);
+  };
   return (
     <div>
+      <div>
+        all
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected("ALL")}
+        />
+        important
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected("IMPORTANT")}
+        />
+        nonimportant
+        <input
+          type="radio"
+          name="filter"
+          onChange={() => filterSelected("NONIMPORTANT")}
+        />
+      </div>
       <h1>Notes</h1>
       <NoteForm />
-      <Notes />
+      <Notes filter={filter} />
     </div>
   );
 };
--- part6/redux-notes/src/components/Notes.jsx
+++ part6/redux-notes/src/components/Notes.jsx
@@ -1,8 +1,37 @@
 import { useSelector, useDispatch } from "react-redux";
 import { toggleImportanceOf } from "../reducers/noteReducer";
-const Notes = () => {
+
+const Notes = ({ filter }) => {
   const dispatch = useDispatch();
-  const notes = useSelector((state) => state);
+  const notes = useSelector((state) => {
+    if (filter === "ALL") {
+      return state;
+    }
+    if (filter === "IMPORTANT") {
+      return state.filter((note) => {
+        if (note.important === true) {
+          return true;
+        }
+      });
+    }
+    if (filter === "NONIMPORTANT") {
+      return state.filter((note) => {
+        if (note.important === false) {
+          return true;
+        }
+      });
+    }
+  });
+  //we can use below as ternary for the above
+  // const notes = useSelector((state) =>
+  //   filter === "ALL"
+  //     ? state
+  //     : filter === "IMPORTANT"
+  //     ? state.filter((note) => note.important === true)
+  //     : filter === "NONIMPORTANT"
+  //     ? state.filter((note) => note.important === false)
+  //     : state
+  // );
 
   const toggleImportant = (id) => {
     dispatch(toggleImportanceOf(id));
