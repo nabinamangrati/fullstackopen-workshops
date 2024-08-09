84eeb8c3b487040f4aee8db282ce760b898eb0df
nabinamangrati <mangratinabina1@gmail.com>
2024-08-09 12:55:50 +0545

    note created using usemutation

--- part6/redux-notes/src/ReactQueryApp.jsx
+++ part6/redux-notes/src/ReactQueryApp.jsx
@@ -1,28 +1,45 @@
-import { useQuery } from "@tanstack/react-query";
-import { getNotes } from "./request";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { getNotes, createNote, updateNote } from "./request";
 const ReactApp = () => {
+  const result = useQuery({
+    queryKey: ["notes"],
+    queryFn: getNotes,
+  });
+  const newNoteMutation = useMutation({
+    mutationFn: createNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+    },
+  });
+
+  const updateNoteMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries("notes");
+    },
+  });
   const addNote = async (event) => {
     event.preventDefault();
     const content = event.target.note.value;
     event.target.note.value = "";
+    newNoteMutation.mutate({ content, important: true });
+
     console.log(content);
   };
 
   const toggleImportance = (note) => {
     console.log("toggle importance of", note.id);
+    updateNoteMutation.mutate(note.id);
   };
 
-  const result = useQuery({
-    queryKey: ["notes"],
-    queryFn: getNotes,
-  });
-  console.log(JSON.parse(JSON.stringify(result)));
+  // console.log(JSON.parse(JSON.stringify(result)));
 
   if (result.isLoading) {
     return <div>loading data...</div>;
   }
 
   const notes = result.data;
+
   return (
     <div>
       <h2>Notes app</h2>
--- part6/redux-notes/src/request.js
+++ part6/redux-notes/src/request.js
@@ -1,7 +1,16 @@
 import axios from "axios";
+const baseUrl = "http://localhost:3001/notes";
 
 export const getNotes = () =>
-  axios.get("http://localhost:3001/api/notes").then((res) => {
-    // for (let i = 0; i < 1000000000; i++) {}
+  axios.get(baseUrl).then((res) => {
+    for (let i = 0; i < 100000000; i++) {}
     return res.data;
   });
+
+export const createNote = (newNote) =>
+  axios.post(baseUrl, newNote).then((res) => res.data);
+
+export const updateNote = (updatedNote) =>
+  axios
+    .put(`${baseUrl}/${updatedNote.id}`, updatedNote)
+    .then((res) => res.data);
