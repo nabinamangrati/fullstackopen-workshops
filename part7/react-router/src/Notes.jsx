2712264ec9e2bfcc2c3c7a5aecbc484d9d15ee73
nabinamangrati <mangratinabina1@gmail.com>
2024-08-13 11:42:05 +0545

    link to id (made dynamic like(/note/2)(/note/5)

--- part7/react-router/src/Notes.jsx
+++ part7/react-router/src/Notes.jsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
 const Notes = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map((note) => (
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? "important" : ""}</strong>
-          </li>
+          <Link to={`/notes/${note.id}`} key={note.id}>
+            <li>
+              {note.content}
+              <strong>{note.important ? "important" : ""}</strong>
+            </li>
+          </Link>
         ))}
       </ul>
     </div>
