e24189e3033759910a5077400edb0298d8faff46
nabinamangrati <mangratinabina1@gmail.com>
2024-08-16 14:17:10 +0545

    navand navbar from react bootstrap

--- part7/react-router/src/App.jsx
+++ part7/react-router/src/App.jsx
@@ -3,7 +3,7 @@ import Notes from "./Notes";
 import Note from "./Note";
 import { useState } from "react";
 import Login from "./Login";
-import { Alert } from "react-bootstrap";
+import { Alert, Nav, Navbar } from "react-bootstrap";
 
 const notes = [
   {
@@ -51,25 +51,38 @@ const App = () => {
   const footerStyle = { color: "blue", fontSize: "30px" };
   return (
     <div className="container">
-      <div>
-        <Link style={padding} to="/">
-          home
-        </Link>
-        <Link style={padding} to="/notes">
-          notes
-        </Link>
-        <Link style={padding} to="/users">
-          users
-        </Link>
-        {user ? (
-          <Alert variant="success">{user} logged in </Alert>
-        ) : (
-          // <em>{user} logged in</em>
-          <Link style={padding} to="/login">
-            login
-          </Link>
-        )}
-      </div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/">
+                home
+              </Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/notes">
+                notes
+              </Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/users">
+                users
+              </Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              {user ? (
+                <Alert variant="success">{user} logged in </Alert>
+              ) : (
+                // <em style={padding}>{user} logged in</em>
+                <Link style={padding} to="/login">
+                  login
+                </Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
       <Routes>
         <Route path="/notes/:id" element={<Note note={note} />} />
