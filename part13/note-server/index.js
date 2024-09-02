dc802c682370c748924d84ce2234ca3f682e7055
nabinamangrati <mangratinabina1@gmail.com>
2024-09-02 17:05:30 +0545

    good practice if the database is connected and then only the server starts

--- part13/note-server/index.js
+++ part13/note-server/index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const notesRouter = require("./controllers/notes");
 const { PORT } = require("./util/config");
+const { connectToDatabase } = require("./util/db");
 
 app.use(express.json());
 app.use("/api/notes", notesRouter);
+const start = async () => {
+  await connectToDatabase();
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+start();
