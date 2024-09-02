43a9704b4e84d13a80d92f4af307648d0e144c60
nabinamangrati <mangratinabina1@gmail.com>
2024-09-02 15:56:53 +0545

    put  and get from id

--- part13/note-server/index.js
+++ part13/note-server/index.js
@@ -52,6 +52,27 @@ app.post("/api/notes", async (req, res) => {
   res.json(note);
 });
 
+app.get("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    console.log(JSON.stringify(note, null, 2));
+    res.json(note);
+  } else {
+    res.status(404).send("no data found");
+  }
+});
+
+app.put("/api/notes/:id", async (req, res) => {
+  const note = await Note.findByPk(req.params.id);
+  if (note) {
+    note.important = req.body.important;
+    await note.save();
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
