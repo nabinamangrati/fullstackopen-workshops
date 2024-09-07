35279c99496fd9f46d1bef5415bc082b63fb55f8
nabinamangrati <mangratinabina1@gmail.com>
2024-09-07 19:09:16 +0545

    users controller added and also these added in index.js fro route

--- part13/note-server/index.js
+++ part13/note-server/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const notesRouter = require("./controllers/notes");
+const loginRouter = require("./controllers/login");
+const usersRouter = require("./controllers/users");
 const { PORT } = require("./util/config");
 const { connectToDatabase } = require("./util/db");
 
 app.use(express.json());
 app.use("/api/notes", notesRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+
 const start = async () => {
   await connectToDatabase();
   app.listen(PORT, () => {
