8b94438cdec5a40769fb7f393a64deff950136ea
nabinamangrati <mangratinabina1@gmail.com>
2024-09-07 19:37:12 +0545

    created note from by the user

--- part13/note-server/controllers/notes.js
+++ part13/note-server/controllers/notes.js
@@ -1,8 +1,25 @@
 const app = require("express").Router();
 const { Note } = require("../models/index");
+const jwt = require("jsonwebtoken");
+
+const { SECRET } = require("../util/config");
 
 const noteFinder = async (req, res, next) => {
   req.note = await Note.findByPk(req.params.id);
+  next();
+};
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
+    } catch {
+      return res.status(401).json({ error: "token invalid" });
+    }
+  } else {
+    return res.status(401).json({ error: "token missing" });
+  }
   next();
 };
 
@@ -11,8 +28,9 @@ app.get("/", async (req, res) => {
   res.json(notes);
 });
 
-app.post("/", async (req, res) => {
+app.post("/", tokenExtractor, async (req, res) => {
   console.log(req.body);
+  req.body.userId = req.decodedToken.id;
   const note = await Note.create(req.body);
   res.json(note);
 });
