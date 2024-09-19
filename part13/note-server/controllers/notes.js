b67a009412ff4c805197bd0bb551b96f2f030858
nabinamangrati <mangratinabina1@gmail.com>
2024-09-19 16:43:34 +0545

    middle ware & admin checking

--- part13/note-server/controllers/notes.js
+++ part13/note-server/controllers/notes.js
@@ -1,26 +1,10 @@
 const app = require("express").Router();
 const { Note, User } = require("../models/index");
-const jwt = require("jsonwebtoken");
 const { Op } = require("sequelize");
-
-const { SECRET } = require("../util/config");
+const { tokenExtractor } = require("../util/middleware");
 
 const noteFinder = async (req, res, next) => {
   req.note = await Note.findByPk(req.params.id);
-  next();
-};
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-    } catch {
-      return res.status(401).json({ error: "token invalid" });
-    }
-  } else {
-    return res.status(401).json({ error: "token missing" });
-  }
   next();
 };
 
