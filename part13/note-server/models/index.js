83894c86f6483f63de49054261284e5c9a1c1ebc
nabinamangrati <mangratinabina1@gmail.com>
2024-09-19 17:25:13 +0545

    team & membership(many to many reln)

--- part13/note-server/models/index.js
+++ part13/note-server/models/index.js
@@ -1,6 +1,12 @@
 const Note = require("./note");
 const User = require("./user");
+const Team = require("./team");
+const Membership = require("./membership");
 
 User.hasMany(Note);
 Note.belongsTo(User);
-module.exports = { Note, User };
+
+User.belongsToMany(Team, { through: Membership });
+Team.belongsToMany(User, { through: Membership });
+
+module.exports = { Note, User, Team, Membership };
