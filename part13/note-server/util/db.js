cf2bc7e538dc8b78de0b4b675eeb4bde4283baac
nabinamangrati <mangratinabina1@gmail.com>
2024-09-19 17:00:57 +0545

    migration rollback

--- part13/note-server/util/db.js
+++ part13/note-server/util/db.js
@@ -11,20 +11,27 @@ const sequelize = new Sequelize(DB_CONNECTION, {
   },
 });
 
+const migrationConf = {
+  migrations: {
+    glob: "migrations/*.js",
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
 const runMigrations = async () => {
-  const migrator = new Umzug({
-    migrations: {
-      glob: "migrations/*.js",
-    },
-    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
-    context: sequelize.getQueryInterface(),
-    logger: console,
-  });
+  const migrator = new Umzug(migrationConf);
 
   const migrations = await migrator.up();
   console.log("Migrations up to date", {
     files: migrations.map((mig) => mig.name),
   });
+};
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate();
+  const migrator = new Umzug(migrationConf);
+  await migrator.down();
 };
 
 const connectToDatabase = async () => {
@@ -40,4 +47,4 @@ const connectToDatabase = async () => {
   return null;
 };
 
-module.exports = { sequelize, connectToDatabase };
+module.exports = { sequelize, connectToDatabase, rollbackMigration };
